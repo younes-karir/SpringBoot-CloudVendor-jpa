d088795029270084f5ecd6d3a5bf4ee430844f91
younes-karir <youneskarir@outlook.com>
2024-02-24 16:31:12 +0100

    testing cloud vendor service

---
+++ src/test/java/com/example/cloudvendorjpa/service/impl/CloudVendorServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.example.cloudvendorjpa.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class CloudVendorServiceImplTest {
+
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCloudVendor() {
+    }
+
+    @Test
+    void updateCloudVendor() {
+    }
+
+    @Test
+    void deleteCloudVendor() {
+    }
+
+    @Test
+    void getCloudVendor() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
